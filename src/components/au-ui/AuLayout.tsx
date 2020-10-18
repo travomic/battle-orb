bc4a17221de9f7fcd202aebd2078286e670f3781
Travis Barney <travis.barney@lifeomic.com>
2020-10-18 12:50:32 -0600

    Basic GraphQL useQuery setup using `urql`.

--- src/components/au-ui/AuLayout.tsx
+++ src/components/au-ui/AuLayout.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import classes from './au-ui.module.styl';
+import { useAuth0 } from '@auth0/auth0-react';
+import { createClient, Provider as QueryProvider } from 'urql';
 
 interface IProps {
+  apiURL: string;
   title: string;
   year: number;
 }
 
-export const AuLayout = ({ 
+export const AuLayout = ({
+  apiURL,
   children, 
   title, 
   year,
 }: React.PropsWithChildren<IProps>) => {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+
+  async function getAuthenticatedHeaders() {
+    return await getAccessTokenSilently().then(accessToken => ({
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      }
+    })
+  )};
+
+  const getAPIOptions = () => isAuthenticated ? getAuthenticatedHeaders() : {};
+
+  const graphQLClient = createClient({
+    url: apiURL,
+    fetchOptions: getAPIOptions()
+  });
+
   return (
-    <div className={classes.root}>
-      <header><h1>{title}</h1></header>
-      <main>{children}</main>
-      <aside></aside>
-      <footer>{year}</footer>
-    </div>
+    <QueryProvider value={graphQLClient}>
+      <div className={classes.root}>
+        <header><h1>{title}</h1></header>
+        <main>{children}</main>
+        <aside></aside>
+        <footer>{year}</footer>
+      </div>
+    </QueryProvider>
   )
 }
--- src/components/user-menu/UserMenu.tsx
+++ src/components/user-menu/UserMenu.tsx
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 interface IProps {
 }
 
-export const UserMenu = ({ }: IProps) => {
+export const UserMenu = ({}: IProps) => {
   const { 
     error,
     isAuthenticated,
