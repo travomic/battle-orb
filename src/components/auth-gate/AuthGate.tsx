210e635bf3577fbff165a2f6868acbce0103ba67
Travis Barney <trave801@gmail.com>
2020-10-30 23:30:26 -0600

    fix: AuthGate return type

--- src/components/auth-gate/AuthGate.tsx
+++ src/components/auth-gate/AuthGate.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 interface IProps {
-  or?: React.ReactNode
+  or?: React.ReactElement
 }
 
-export const AuthGate = ({ children, or }: React.PropsWithChildren<IProps>) => {
+export const AuthGate = ({ children, or }: React.PropsWithChildren<IProps>): React.ReactElement => {
   const { isAuthenticated } = useAuth0();
   return isAuthenticated ? <>{children}</> : or ? or : <></>;
 }
